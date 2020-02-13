92937d9ba96c9a1f1ba99f8dbc9ff06d6b47ed8f
Simon Hayoz <hayoz.simon@bluewin.ch>
2020-02-13 15:02:24 +0100

    implements blank lines test

---
+++ codeEval/src/main/java/eval/code/quality/Position.java
@@ -0,0 +1,17 @@
+package eval.code.quality;
+
+public class Position {
+    public final int line;
+    public final int column;
+
+    public Position(int line, int column) {
+        this.line = line;
+        this.column = column;
+    }
+
+    @Override
+    public String toString() {
+        return "line: " + line + " - column: " + column;
+    }
+  
+  }
---
+++ codeEval/src/main/java/eval/code/quality/Range.java
@@ -0,0 +1,16 @@
+package eval.code.quality;
+
+public class Range {
+    public final Position begin;
+    public final Position end;
+
+    public Range(Position begin, Position end) {
+        this.begin = begin;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + begin + ", " + end + ")";
+    }
+}
