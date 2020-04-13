dc9447d850f8fee27f61edc73971c24aeea4e62c
Simon Hayoz <hayoz.simon@bluewin.ch>
2020-04-13 17:16:29 +0200

    add tests for TestSuite

--- src/main/java/eval/code/quality/tests/TestSuite.java
+++ src/main/java/eval/code/quality/tests/TestSuite.java
@@ -1,5 +1,7 @@
 package eval.code.quality.tests;
 
+import eval.code.quality.utils.Preconditions;
+
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -13,18 +15,28 @@ public class TestSuite {
     private final Map<String, Report> testResults;
 
     public TestSuite(List<Test> tests) {
+        Preconditions.checkArg(tests != null, "The test suite can not be null");
         this.tests = tests;
         this.testResults = new HashMap<>();
     }
 
+    /**
+     * Run all tests in the test suite.
+     * @return the map of test -> report
+     */
     public Map<String, Report> runTests() {
         return runTests(false);
     }
 
+    /**
+     * Run all tests with or without verbose.
+     * @param verbose whether the test should print debug verbose or not
+     * @return the map of test -> report
+     */
     public Map<String, Report> runTests(boolean verbose) {
         testResults.clear();
         for(Test test: tests) {
-            testResults.put(test.getName(), test.run());
+            testResults.put(test.getName(), test.run(verbose));
         }
         return testResults;
     }
