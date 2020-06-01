579c85bae407b1741c9a87c36c32ac5f00f46e24
Simon Hayoz <hayoz.simon@bluewin.ch>
2020-06-01 11:23:23 +0200

    Add warning to design pattern for naming convention (visit, build, etc)

--- src/main/java/eval/code/quality/checks/pattern/VisitorPattern.java
+++ src/main/java/eval/code/quality/checks/pattern/VisitorPattern.java
@@ -1,6 +1,7 @@
 package eval.code.quality.checks.pattern;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
 import eval.code.quality.provider.ContentProvider;
 import eval.code.quality.checks.DesignPattern;
 import eval.code.quality.utils.evaluator.BooleanEvaluator;
@@ -41,17 +42,20 @@ public class VisitorPattern extends DesignPattern {
         ClassOrInterfaceDeclaration visitor = contentProvider.findClassBy(visitorName).orElseThrow(() -> new ClassNotFoundException(visitorName));
         BooleanEvaluator evaluator = new BooleanEvaluator();
         List<String> childrenSimpleName = childrenName.stream().map(this::getSimpleName).collect(Collectors.toList());
-        List<String> visitorMethodsParametersName = visitor.getMethods().stream()
-                .filter(method -> method.getNameAsString().equals("visit"))
+        List<MethodDeclaration> visitorMethod = visitor.getMethods().stream()
+                .filter(method -> method.getParameters().stream()
+                        .map(parameter -> parameter.getType().asString())
+                        .anyMatch(childrenSimpleName::contains)).collect(Collectors.toList());
+        List<String> visitorMethodsParametersName = visitorMethod.stream()
                 .map(method -> method.getParameters().stream()
                         .map(parameter -> parameter.getType().asString())
-                        .filter(childrenSimpleName::contains)
                         .findFirst().orElseThrow())
                 .collect(Collectors.toList());
-        evaluator.add(() -> visitorMethodsParametersName.containsAll(childrenSimpleName), "visitor has a method \"visit\" for all child");
+        evaluator.add(() -> visitorMethodsParametersName.containsAll(childrenSimpleName), "visitor has a visit method for all child");
         BooleanExpression hasAcceptMethodParent = new BooleanSimple(() -> hasAcceptMethodWithVisitor(parent), "Parent interface has accept method to be implemented by children");
         BooleanExpression hasAcceptMethodChildren = new BooleanSimple(() -> children.stream().allMatch(this::hasAcceptMethodWithVisitor), "Every children implements accept method");
         evaluator.add(new BooleanOr(hasAcceptMethodParent, hasAcceptMethodChildren));
+        evaluator.add(new BooleanSimple(() -> visitorMethod.stream().allMatch(m -> m.getNameAsString().equals("visit")), "Visit method are called 'visit'", false));
         return evaluator;
     }
 
